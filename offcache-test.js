f4810568dbab7179bd6a411ab8264b5824993ec7
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-28 16:02:47 +0800

    Updated server-side test.

--- offcache-test.js
+++ offcache-test.js
@@ -1,5 +1,17 @@
-"use strict";
 
+const assert = require( "assert" );
 const offcache = require( "./offcache.js" );
 
-offcache( );
+const http = require( "http" );
+
+http.createServer( function ( request, response ){
+
+	console.log( offcache( response ) );
+
+	response.writeHead( 200, { "Content-Type": "text/plain" } );
+
+	response.write( "Hello World!" );
+
+	response.end( );
+
+} ).listen( "8080", "127.0.0.1" );
